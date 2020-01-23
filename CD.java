a241e1efcb463aa7bfb5e0992607f0f0453083bd
devanshaggarwal026 <50549721+devanshaggarwal026@users.noreply.github.com>
2020-01-23 17:31:18 +0530

    Update CD.java

--- CD.java
+++ CD.java
@@ -1,4 +1,45 @@
 
-public class CD {
 
+public class CD extends MediaItem{
+	
+	private String artist , genre ;
+	
+	public CD(int uniqueIdNo , String title , int noOfCopies , int runtime , String artist , String genre) {
+		super(uniqueIdNo , title , noOfCopies , runtime);
+		this.genre = genre;
+		this.artist = artist;
+	}
+	
+	
+	
+	public int computeSalePrice() {
+		return super.getNoOfCopies();
+	}
+	
+	
+	public void random() {
+		//return super.getRuntime();
+	}
+
+
+	public String getArtist() {
+		return artist;
+	}
+
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
 }
