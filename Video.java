7d882dcb7c90b9645fc14058c8de1c29757a7532
devanshaggarwal026 <50549721+devanshaggarwal026@users.noreply.github.com>
2020-01-23 17:34:19 +0530

    Update Video.java

--- Video.java
+++ Video.java
@@ -4,7 +4,7 @@ public class Video extends MediaItem {
 	private String director , genre ;
 	private int year;
 	
-	private Video(int uniqueIdNo , String title , int noOfCopies , int runtime , String director , String genre ,  int Year) {
+	Video(int uniqueIdNo , String title , int noOfCopies , int runtime , String director , String genre ,  int Year) {
 		super(uniqueIdNo , title , noOfCopies , runtime);
 		this.director = director;
 		this.genre = genre;
