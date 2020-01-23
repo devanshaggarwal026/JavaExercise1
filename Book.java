fd3abf8dcfa82c35c55c303a8fc2c7a7a4c841c9
devanshaggarwal026 <50549721+devanshaggarwal026@users.noreply.github.com>
2020-01-23 17:30:22 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,4 +1,14 @@
 
-public class Book {
-
+public class Book extends WrittenItem {
+	
+	public Book(int uniqueIdNo , String title , int noOfCopies , String author) {
+		super(uniqueIdNo , title , noOfCopies , author);
+	}
+	
+	public String physicalLibrary() {
+		return super.getAuthor();
+	}
+	
+	
 }
+
