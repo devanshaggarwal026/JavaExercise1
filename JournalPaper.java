150c18e3205072eed94764189f310e872c4c54f2
devanshaggarwal026 <50549721+devanshaggarwal026@users.noreply.github.com>
2020-01-23 17:32:07 +0530

    Update JournalPaper.java

--- JournalPaper.java
+++ JournalPaper.java
@@ -1,4 +1,25 @@
 
-public class JournalPaper {
+public class JournalPaper extends WrittenItem {
 
+	private int yearPublished;
+	
+	public JournalPaper(int uniqueIdNo , String title , int noOfCopies , String author , int yearPublished) {
+		super(uniqueIdNo , title , noOfCopies , author);
+		this.yearPublished = yearPublished;
+	}
+	
+	public String physicalLibrary() {
+		return super.getAuthor();
+	}
+
+	public int getYearPublished() {
+		return yearPublished;
+	}
+
+	public void setYearPublished(int yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+	
+	
 }
+
